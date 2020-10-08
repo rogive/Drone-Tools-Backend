553e3382ff6ec8f8462b99f4a1b4c8deafec276e
ivan <ierodriguezgo@gmail.com>
2020-10-08 07:02:20 -0500

    arquitecture mvc

---
+++ src/database/connection.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+require("dotenv").config();
+
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+  host: process.env.HOST,
+  dialect: process.env.DIALECT
+});
+
+module.exports = sequelize;
